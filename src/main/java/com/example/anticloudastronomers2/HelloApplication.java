e343ed362491a761996da4b15a6913aac2d31a8d
Greg Frazer <gregfrazer99@gmail.com>
2022-05-17 15:51:54 +0100

    added variables for all the labels and images planned on the home page

--- src/main/java/com/example/anticloudastronomers2/HelloApplication.java
+++ src/main/java/com/example/anticloudastronomers2/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainPane.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
         stage.setTitle("Hello there test commit!");
         stage.setScene(scene);
         stage.show();
