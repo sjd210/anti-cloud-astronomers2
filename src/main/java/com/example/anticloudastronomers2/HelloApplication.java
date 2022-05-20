dd1c2d46cfc31833c7760a5750ab75e37eabbab4
Sol Dubock <sjd210@cam.ac.uk>
2022-05-20 13:21:21 +0100

    Map + adjusting screen sizes + button icons

--- src/main/java/com/example/anticloudastronomers2/HelloApplication.java
+++ src/main/java/com/example/anticloudastronomers2/HelloApplication.java
@@ -13,8 +13,8 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("MainPane.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
-        stage.setTitle("Hello there test commit!");
+        Scene scene = new Scene(fxmlLoader.load(), 335, 600);
+        stage.setTitle("Very good weather app");
         stage.setScene(scene);
         stage.show();
 
